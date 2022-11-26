af8e0dcaab6db33dc4d6532e5b704bba9a45e2e7
Jagmandeep Dhaliwal <jagman.dhaliwal@gmail.com>
2022-11-26 10:15:40 -0500

    added loader

--- script.js
+++ script.js
@@ -3,13 +3,30 @@ const quoteText = document.getElementById('quote');
 const authorText =  document.getElementById('author');
 const newQuoteButton = document.getElementById('new-quote');
 const twitterButton = document.getElementById('twitter');
+const loader = document.getElementById('loader');
+const quoteContainer = document.getElementById('quote-container');
+
+function loading(){
+    // using the hidden function to hide the loader, when we don't need to display it
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function complete(){
+    
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+    
+}
+
 
 // Get Quotes From API
 
 function displayquotes(quotes){
-
+        loading();
         quoteText.textContent = quotes.text;
         authorText.textContent = quotes.author;
+        complete();
 
 }
 
@@ -24,16 +41,17 @@ async function getQuotes(){
     } catch(e){
         console.log(e)
     }
+   
 }
 
 getQuotes()
-
-
 
 function tweetQuote(){
     const twitterURl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
     window.open(twitterURl, '_blank');
 }
+
+
 
 newQuoteButton.addEventListener('click', getQuotes)
 
